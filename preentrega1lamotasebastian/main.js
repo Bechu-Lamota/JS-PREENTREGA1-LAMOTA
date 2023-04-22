46d9db170e938084e6ca72c21f056486a059e8c8
sebastianlamota <lamota.as@gmail.com>
2023-04-22 11:32:28 -0300

    rama_uno

---
+++ preentrega1lamotasebastian/main.js
@@ -0,0 +1,46 @@
+
+
+let edad = parseInt(prompt("edad"));
+
+if (edad < 16) {
+	alert("no podes entrar");
+} else if (edad < 17) {
+	alert("pasas, pero no te creo nada");
+} else {
+	alert("adelante");
+}
+
+
+
+
+
+function calculadora(num1, num2, operacion) {
+	switch (operacion) {
+		case "+":
+			return num1 + num2;
+			break;
+
+		case "-":
+			return num1 - num2;
+			break;
+
+		case "*":
+			return num1 * num2;
+			break;
+
+		case "/":
+			return num1 / num2;
+			break;
+
+		default:
+			return "Operacion no encontrada";
+			break;
+	}
+}
+
+let num1 = parseInt(prompt("Ingrese el numero"));
+let num2 = parseInt(prompt("Ingrese el numero"));
+let operacion = prompt("Ingrese el operador");
+
+let resultado = calculadora(num1, num2, operacion);
+alert(`El resultado es ${resultado}`);
