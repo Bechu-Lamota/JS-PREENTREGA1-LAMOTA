6a321ca01b8ec78e15543f750ca77400b6743775
sebastianlamota <lamota.as@gmail.com>
2023-04-22 11:46:18 -0300

    rama_uno

--- main.js
+++ main.js
@@ -4,37 +4,66 @@ if (edad < 16) {
 	alert("no podes entrar");
 } else if (edad < 17) {
 	alert("pasas, pero no te creo nada");
+	function calculadora(num1, num2, operacion) {
+		switch (operacion) {
+			case "+":
+				return num1 + num2;
+				break;
+
+			case "-":
+				return num1 - num2;
+				break;
+
+			case "*":
+				return num1 * num2;
+				break;
+
+			case "/":
+				return num1 / num2;
+				break;
+
+			default:
+				return "Operacion no encontrada";
+				break;
+		}
+	}
+
+	let num1 = parseInt(prompt("Ingrese el numero"));
+	let num2 = parseInt(prompt("Ingrese el numero"));
+	let operacion = prompt("Ingrese el operador");
+
+	let resultado = calculadora(num1, num2, operacion);
+	alert(`El resultado es ${resultado}`);
 } else {
 	alert("adelante");
-}
+	function calculadora(num1, num2, operacion) {
+		switch (operacion) {
+			case "+":
+				return num1 + num2;
+				break;
 
-function calculadora(num1, num2, operacion) {
-	switch (operacion) {
-		case "+":
-			return num1 + num2;
-			break;
+			case "-":
+				return num1 - num2;
+				break;
 
-		case "-":
-			return num1 - num2;
-			break;
+			case "*":
+				return num1 * num2;
+				break;
 
-		case "*":
-			return num1 * num2;
-			break;
+			case "/":
+				return num1 / num2;
+				break;
 
-		case "/":
-			return num1 / num2;
-			break;
-
-		default:
-			return "Operacion no encontrada";
-			break;
+			default:
+				return "Operacion no encontrada";
+				break;
+		}
 	}
+
+	let num1 = parseInt(prompt("Ingrese el numero"));
+	let num2 = parseInt(prompt("Ingrese el numero"));
+	let operacion = prompt("Ingrese el operador");
+
+	let resultado = calculadora(num1, num2, operacion);
+	alert(`El resultado es ${resultado}`);
 }
-
-let num1 = parseInt(prompt("Ingrese el numero"));
-let num2 = parseInt(prompt("Ingrese el numero"));
-let operacion = prompt("Ingrese el operador");
-
-let resultado = calculadora(num1, num2, operacion);
-alert(`El resultado es ${resultado}`);
