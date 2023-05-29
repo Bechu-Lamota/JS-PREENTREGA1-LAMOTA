2ff32fbe8ee07113e16b4951f3fc24504e7e78a1
sebastianlamota <lamota.as@gmail.com>
2023-05-29 16:57:49 -0300

    rama_uno

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 //ENTREGA 3
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("miForm").addEventListener("submit", function (event) {
+		event.preventDefault();
+		// Evito que se envíen los datos automáticamente.
 
-document.getElementById("miFormulario").addEventListener("submit", function (event) {
-	event.preventDefault();
-	// Evito que se envíen los datos automáticamente.
+		// Obtengo los valores de los campos del formulario
+		let nombre = document.getElementById("nombre").value;
+		let apellido = document.getElementById("apellido").value;
+		let email = document.getElementById("email").value;
+		let contraseña = document.getElementById("contraseña").value;
 
-	// Obtengo los valores de los campos del formulario
-	let nombre = document.getElementById("nombre").value;
-	let apellido = document.getElementById("apellido").value;
-	let email = document.getElementById("email").value;
-	let contraseña = document.getElementById("contraseña").value;
+		//Los unifico
+		let usuarios = {
+			nombre: nombre,
+			apellido: apellido,
+			email: email,
+			contraseña: contraseña
+		};
 
-	//Los unifico
-	let usuarios = {
-		nombre: nombre,
-		apellido: apellido,
-		email: email,
-		contraseña: contraseña
-	};
+		localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-	localStorage.setItem("usuarios", JSON.stringify(usuarios));
-
-	console.log(`Datos Almacenados en localStorage:`);
-	console.log(usuarios);
+		console.log(`Datos Almacenados en localStorage:`);
+		console.log(usuarios);
+	});
 });
